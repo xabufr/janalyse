26b9f98e4ef545cf7f2cd7e128fc1dcf13a30098
Thomas LOUBIOU <tloubiou@epsi.fr>
2012-10-08 14:28:35 +0200

    CMD logique héritage, relation, cardinalite

--- src/com/mcd_log/auth/Relation.java
+++ src/com/mcd_log/auth/Relation.java
@@ -2,6 +2,24 @@ package com.mcd_log.auth;
 
 public class Relation {
 	public Relation(){
-		
+		this.m_cardinalites = new Cardinalite[2];
+		this.m_cardinalites[0] = new Cardinalite();
+		this.m_cardinalites[1] = new Cardinalite();
 	}
+	public String getNom() {
+		return m_nom;
+	}
+	public void setNom(String nom) {
+		this.m_nom = nom;
+	}
+	public String getCommentaire() {
+		return m_commentaire;
+	}
+	public void setCommentaire(String m_commentaire) {
+		this.m_commentaire = m_commentaire;
+	}
+	private String m_nom;
+	private String m_commentaire;
+	private Cardinalite m_cardinalites[];
+	
 }
