2e394755a9c5bb05a5f5d2d44baf5945d1a1d4c6
zequiel <zequiel@aramel@epsi.fr>
2012-10-08 17:33:26 +0200

    Ajout déplacement entité

--- src/com/mcd_composent_graph/auth/EntiteGraph.java
+++ src/com/mcd_composent_graph/auth/EntiteGraph.java
@@ -1,9 +1,38 @@
 package com.mcd_composent_graph.auth;
 
-public class EntiteGraph {
+import java.awt.Color;
+import java.awt.Graphics;
 
-	public EntiteGraph() {
-		// TODO Auto-generated constructor stub
+public class EntiteGraph extends Object implements McdComposentGraphique{
+	private int x;
+	private int y;
+	
+	public EntiteGraph(int x, int y) {
+		setX(x);
+		setY(y);
 	}
 
+	public void Dessiner(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillRect(x, y, 120, 130);
+		g.setColor(Color.BLACK);
+		g.drawRect(x, y, 120, 130);
+		g.drawLine(x, (y+30), (x+120), (y+30));
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
