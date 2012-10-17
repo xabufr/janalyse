6a6f8e74cd5bf643fab6c5647659bd96ef40e44d
Thomas LOUBIOU <tloubiou@epsi.fr>
2012-10-17 15:39:30 +0200

    merge

--- src/com/mcd_graph/auth/FenetrePrincipale.java
+++ src/com/mcd_graph/auth/FenetrePrincipale.java
@@ -434,34 +434,20 @@ public class FenetrePrincipale {
 			}
 		});
 		frame.addWindowListener(new WindowListener() {
-			public void windowOpened(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowOpened(WindowEvent arg0) {				
 			}
-			public void windowIconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowIconified(WindowEvent arg0) {				
 			}
-			public void windowDeiconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowDeiconified(WindowEvent arg0) {				
 			}
-			public void windowDeactivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowDeactivated(WindowEvent arg0) {				
 			}
-			
 			public void windowClosing(WindowEvent arg0) {
 				quitter();
-				
 			}
-			public void windowClosed(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowClosed(WindowEvent arg0) {				
 			}
-			public void windowActivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void windowActivated(WindowEvent arg0) {				
 			}
 		});
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
