43c4966ecfa3c9707fd5221a11843baf7e769054
Thomas LOUBIOU <tloubiou@epsi.fr>
2012-10-17 20:15:04 +0200

    Amélioration à propos

--- src/com/ui_help/auth/APropos.java
+++ src/com/ui_help/auth/APropos.java
@@ -5,12 +5,19 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JTextPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SuppressWarnings("serial")
 public class APropos extends JDialog {
@@ -40,7 +47,7 @@ public class APropos extends JDialog {
 			public void mouseClicked(MouseEvent arg0) {				
 			}
 		};
-		setTitle("A Propos");
+		setTitle("À Propos");
 		setModal(true);
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setBounds(100, 100, 450, 300);
@@ -53,8 +60,25 @@ public class APropos extends JDialog {
 		JTextPane txtpnJanalyseCrateurDe = new JTextPane();
 		txtpnJanalyseCrateurDe.setEditable(false);
 		txtpnJanalyseCrateurDe.setContentType("text/html");
-		txtpnJanalyseCrateurDe.setText("JAnalyse, créateur de MCD Merise.\n<br />\nÉcrit par:<br />\n<ul>\n<li>Alexandre Rame(<a href=\"mailto:aramel@epsi.fr\">aramel@epsi.fr</a>)</li>\n<li>Thomas Loubiou(<a href=\"mailto:tloubiou@epsi.fr\">tloubiou@epsi.fr</a>)</li>\n</ul>");
+		txtpnJanalyseCrateurDe.setText("JAnalyse, créateur de MCD Merise.\n<br />\nÉcrit par:<br />\n<ul>\n<li>Alexandre Rame(<a href=\"mailto:aramel@epsi.fr\">aramel@epsi.fr</a>)</li>\n<li>Thomas Loubiou(<a href=\"mailto:tloubiou@epsi.fr\">tloubiou@epsi.fr</a>)</li>\n</ul>\nVous pouvez consulter l'avancement du projet sur:<br />\n<a href=\"https://www.assembla.com/code/janalyse/\">https://www.assembla.com/code/janalyse/</a>");
 		panel.add(txtpnJanalyseCrateurDe, "cell 0 0,grow");
+		
+		txtpnJanalyseCrateurDe.addHyperlinkListener(new HyperlinkListener() {
+			
+			public void hyperlinkUpdate(HyperlinkEvent e) {
+				if(!Desktop.isDesktopSupported())
+					return;
+				if(e.getEventType()!=HyperlinkEvent.EventType.ACTIVATED)
+					return;
+				try {
+					Desktop.getDesktop().browse(new URI(e.getURL().getPath()));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		
 		JLabel lblEscOuClick = new JLabel("Esc. ou click pour quitter");
 		lblEscOuClick.setFont(new Font("Dialog", Font.PLAIN, 12));
@@ -65,7 +89,6 @@ public class APropos extends JDialog {
 		addKeyListener(keylist);
 		addMouseListener(mouselist);
 		txtpnJanalyseCrateurDe.addKeyListener(keylist);
-		txtpnJanalyseCrateurDe.addMouseListener(mouselist);
 		panel.addKeyListener(keylist);
 		panel.addMouseListener(mouselist);
 		lblEscOuClick.addKeyListener(keylist);
