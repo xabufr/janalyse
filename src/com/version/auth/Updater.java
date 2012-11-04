949d2b56b90693fd061d0fe16a68fae46fef11b5
Thomas LOUBIOU <tloubiou@epsi.fr>
2012-11-04 14:18:24 +0100

    Lien version.xml MAJ

--- src/com/version/auth/Updater.java
+++ src/com/version/auth/Updater.java
@@ -148,7 +148,7 @@ public class Updater {
  
 	}
 	static private int m_currentVersion = 1;
-	//static private String m_urlVersion="https://www.assembla.com/code/janalyse/git/nodes/master/src/com/version/auth/version.xml";
-	static private String m_urlVersion="file:///home/thomas/workspace/janalyse/src/com/version/auth/version.xml";
+	static private String m_urlVersion="https://www.assembla.com/code/janalyse/git/nodes/master/src/com/version/auth/version.xml";
+	//static private String m_urlVersion="file:///home/thomas/workspace/janalyse/src/com/version/auth/version.xml";
 
 }
