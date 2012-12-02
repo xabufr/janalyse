b1e7e1cb7552c007835985249e76dd6d033b243a
Thomas LOUBIOU <tloubiou@epsi.fr>
2012-12-02 15:04:58 +0100

    Ajout type bool

--- src/com/mcd_log/auth/ProprieteTypeE.java
+++ src/com/mcd_log/auth/ProprieteTypeE.java
@@ -4,6 +4,7 @@ public enum ProprieteTypeE {
 	NONE(0),
 	INT(1), BIGINT(1), SMALLINT(1), TINYINT(1),
 	VARCHAR(1), CHAR(1),
+	BOOL(0), BOOLEAN(0),
 	FLOAT(2), REAL(2), DOUBLE(2), 
 	DATE(0), DATETIME(0), TIME(0), TIMESTAMP(0),
 	TEXT(0), BLOB(0);
